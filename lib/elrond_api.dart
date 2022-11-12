8bf0fa68a870dedde8b099178a438b29d48a6ad8
Kevin Segaud <kevin.segaud@canb.eu>
2022-11-12 14:08:14 +0100

    add dapp/config (#104)

--- lib/elrond_api.dart
+++ lib/elrond_api.dart
@@ -3,20 +3,24 @@ library elrond_api;
 import 'dart:convert';
 
 import 'package:elrond_api/src/contants.dart';
+import 'package:elrond_api/src/repositories/dapp.dart';
 import 'package:elrond_api/src/repositories/response/response.dart';
 import 'package:http/http.dart';
 
 export 'package:elrond_api/src/contants.dart';
 export 'package:elrond_api/src/repositories/response/response.dart';
+export 'package:elrond_api/src/repositories/response/dapp/get_config/get_config.dart';
 
 class ElrondApi {
   final Client client;
   final String baseUrl;
 
+  final Dapp dapp;
+
   ElrondApi({
     required this.client,
     this.baseUrl = mainnetApiBaseUrl,
-  });
+  }) : dapp = Dapp(baseUrl, client);
 
   Future<String> hello() async {
     final response = await client.get(Uri.parse('$baseUrl/hello'));
